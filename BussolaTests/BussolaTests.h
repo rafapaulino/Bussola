30a15f000923ddfd7d1a71dbdbb858d62b04cf56
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-17 11:25:17 -0300

    Initial Commit

---
+++ BussolaTests/BussolaTests.h
@@ -0,0 +1,13 @@
+//
+//  BussolaTests.h
+//  BussolaTests
+//
+//  Created by Rafael Brigagão Paulino on 17/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BussolaTests : SenTestCase
+
+@end
---
+++ BussolaTests/BussolaTests.m
@@ -0,0 +1,32 @@
+//
+//  BussolaTests.m
+//  BussolaTests
+//
+//  Created by Rafael Brigagão Paulino on 17/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "BussolaTests.h"
+
+@implementation BussolaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BussolaTests");
+}
+
+@end
